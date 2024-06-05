649337a6eb86ae7a3a21e886cd9a71ca00b637d0
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-06-06 02:14:39 +0600

    chore: logo resize

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Logo = ({ footer }) => {
   return (
     <Link to={"/"}
-      className={`font-logo text-xl font-bold ${
-        footer ? "text-[#fefdf8]" : "text-[#5ab270]"
+      className={`font-logo font-bold ${
+        footer ? "text-[#fefdf8] text-xl" : "text-[#5ab270] text-2xl md:text-3xl"
       }`}
     >
       <span className="text-[#f3c556]">Hadith</span>Khujo
