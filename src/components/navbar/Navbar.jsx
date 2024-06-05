183cd9d2f77fd0038ce8b16702d9edbf4b3ac6af
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-06-06 01:57:48 +0600

    sidebar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,58 +1,24 @@
 import Logo from "../logo/Logo";
 import NavItems from "./NavItems";
-import { HiOutlineBell } from "react-icons/hi2";
 import profileImg from "./../../assets/profileImg.png";
 import { MdOutlineDarkMode } from "react-icons/md";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { MdOutlineLightMode } from "react-icons/md";
-import { useDispatch, useSelector } from "react-redux";
-import { changeToBn } from "../../redux/features/languageSlice";
-import { changeToEn } from "../../redux/features/languageSlice";
+import Sidebar from "../sidebar/Sidebar";
+import { IoMenuOutline } from "react-icons/io5";
 
-const Navbar = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-
-  // dark theme logic
-  useEffect(() => {
-    const mainDiv = document.getElementById("main-div");
-    if (theme === "dark") {
-      mainDiv.classList.add("dark");
-      mainDiv.classList.remove("light");
-    } else {
-      mainDiv.classList.add("light");
-      mainDiv.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const handleThemeChange = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
-    localStorage.setItem("theme", newTheme);
-    setTheme(newTheme);
-  };
-
-  // bangla / english
-  const { lang } = useSelector((state) => state.language);
-  const dispatch = useDispatch();
-
-  ;
-  useEffect(() => {
-    localStorage.setItem("lang", lang);  
-  }, [lang]);
-
-  const handleChangeLanguage = () => {
-    lang === "en" ? dispatch(changeToBn()) : dispatch(changeToEn());
-  };
+const Navbar = ({handleChangeLanguage, lang , handleThemeChange, theme}) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  
   return (
     <>
       <div className="text-[#0e1037] py-1 dark:bg-stone-700 dark:text-[#fefdf8] bg-[#fff9ef] dark:shadow-[0_2px_20px_rgba(0,0,0,0.17)] shadow-[0_2px_10px_rgba(0,0,0,0.12)] fixed top-0 left-0 w-full z-50">
-        <div className="xl:container mx-auto px-8 flex justify-between items-center">
+        <div className="xl:container mx-auto px-8  h-[64px] flex justify-between items-center">
           <Logo />
-          <NavItems />
-          <div className="flex items-center gap-4">
-            <HiOutlineBell
-              size={24}
-              className=" cursor-pointer text-[#0e1037] dark:text-[#fefdf8] hover:scale-110 duration-100 transform"
-            />
+          <ul className="hidden sm:flex  gap-8 py-5 text-base font-semibold">
+            <NavItems setShow={setShowSidebar}/>
+          </ul>
+          <div className="hidden sm:flex  items-center gap-4">
             <div className="cursor-pointer" onClick={handleChangeLanguage}>
               {lang === "en" ? "বাংলা" : "English"}
             </div>
@@ -71,8 +37,13 @@ const Navbar = () => {
                 <img src={profileImg} alt="profile-img" />
               </div>
             </div>
+            
           </div>
+          <div onClick={()=>setShowSidebar(true)} className="border-[2px] sm:hidden border-[#585858] dark:border-white rounded-md px-1 py-0.5">
+              <IoMenuOutline size={26} color={theme == 'dark' ? "white":"#444444"}/>
+            </div>
         </div>
+        <Sidebar theme={theme} handleThemeChange={handleThemeChange} lang={lang} handleChangeLanguage={handleChangeLanguage} show={showSidebar} handleShow={setShowSidebar}/>
       </div>
     </>
   );
