9a0f3788744ac5a79136bfa5e6a2298587cbc78f
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-06-14 19:12:19 +0600

    chore: minor changes

--- src/components/bookOfHadith/HadithCards.jsx
+++ src/components/bookOfHadith/HadithCards.jsx
@@ -6,7 +6,7 @@ const HadithCards = () => {
   const [hadithBooks, setHadithBooks] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("/hadithbooks.json");
+      const res = await fetch("http://localhost:5000/hadith-books");
       const data = await res.json();
       if (data?.length > 0) {
         setHadithBooks(data);
@@ -20,7 +20,7 @@ const HadithCards = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3  gap-10 mt-10  justify-between">
       {hadithBooks.length > 0
-        ? hadithBooks.map((book, idx) => <div key={idx} data-aos="fade-up" data-aos-duration="500" data-aos-delay={`${((idx+1) *2 )*50}`}><Card  book={book} /></div>)
+        ? hadithBooks.map((book, idx) => <div key={idx} data-aos="fade-up" data-aos-duration="500" data-aos-delay={`${((idx+1) *2 )*50}`}><Card  book={book} hadithbooks={true}/></div>)
         : null}
     </div>
   );
