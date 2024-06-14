f3aebd7c0b0469ce7ace03b7cb7e0dca09b67fb5
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-06-14 20:11:50 +0600

    feat:add hadith book done using react form hook

--- src/pages/hadithBooks/HadithBooksPage.jsx
+++ src/pages/hadithBooks/HadithBooksPage.jsx
@@ -4,7 +4,7 @@ import MainLayout from "../../layout/MainLayout";
 const HadithBooksPage = () => {
   return (
     <MainLayout>
-      <div className="min-h-[calc(100vh-144px)] text-[#0e1037] dark:text-[#fefdf8] flex justify-center items-center text-6xl font-secondary">
+      <div className="min-h-[calc(100vh-146px)] text-[#0e1037] dark:text-[#fefdf8] flex justify-center items-center text-6xl font-secondary">
         Hadith books page is under development.
       </div>
     </MainLayout>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 import HadithPage from "../pages/hadith/HadithPage";
 import HadithBooksPage from "../pages/hadithBooks/HadithBooksPage";
+import AddHadithBook from "../dashboard/pages/AddHadithBook";
 
 export const routes = createBrowserRouter([
   {
     index: true,
     element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    element: <AddHadithBook/>,
   },
   {
     path: "/hadith",
